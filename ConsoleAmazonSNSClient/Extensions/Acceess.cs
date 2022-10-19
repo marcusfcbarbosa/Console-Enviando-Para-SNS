f42ac533839ab02840906f382f1ee5bffae0bf3b
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2022-10-19 10:04:46 -0300

    Add project files.

---
+++ ConsoleAmazonSNSClient/Extensions/Acceess.cs
@@ -0,0 +1,37 @@
+﻿using Amazon.Runtime;
+using Amazon.SQS;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConsoleAmazonSNSClient.Extensions
+{
+    public static class Acceess
+    {
+        public static string GetRoute()
+        {
+            return GetSeetings().GetValue<string>("Queue");
+        }
+        public static string GetTopiARN()
+        {
+            return GetSeetings().GetValue<string>("SNS_ARN");
+        }
+        public static IConfigurationRoot GetSeetings()
+        {
+            return new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsetttings.json")
+               .AddEnvironmentVariables() 
+               .Build();
+        }
+        public static AmazonSQSClient GetCredentials()
+        {
+            var accessKey = GetSeetings().GetValue<string>("AccessKey");
+            var secret = GetSeetings().GetValue<string>("Secret");
+            var credential = new BasicAWSCredentials(accessKey, secret);
+            return new AmazonSQSClient(credentials: credential, region: Amazon.RegionEndpoint.SAEast1);
+        }
+    }
+}
---
+++ ConsoleAmazonSNSClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using ConsoleAmazonSNSClient.Extensions;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleAmazonSNSClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new AmazonSimpleNotificationServiceClient(Amazon.RegionEndpoint.SAEast1);
+            SendMessage(client).Wait();
+        }
+        static async Task SendMessage(IAmazonSimpleNotificationService snsClient)
+        {
+            try
+            {
+                int count = 20;
+                do
+                {
+                    var request = new PublishRequest
+                    {
+                        TopicArn = Acceess.GetTopiARN(),
+                        Message = JsonSerializer.Serialize(new EnvioTitulo
+                        {
+                            idLinha = Guid.NewGuid().ToString(),
+                            codEspecieDoc = "2342223232",
+                            seuNumero = "122233455555",
+                            dataVencimento = DateTime.Now
+                        }),Subject= $"ENVIO TITUTLOS MARCUS {DateTime.Now.ToString()}"
+                    };
+                    await snsClient.PublishAsync(request);
+                    count--;
+                } while (count > 0);
+               
+
+            }
+            catch(Exception ex)
+            {
+                string erro = ex.Message;
+            }
+           
+        }
+        public class EnvioTitulo
+        {
+            public string idLinha { get; set; }
+            public string codEspecieDoc { get; set; }
+            public string seuNumero { get; set; }
+            public DateTime dataVencimento { get; set; }
+
+        }
+        
+    }
+}
+
